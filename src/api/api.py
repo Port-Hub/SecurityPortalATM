2f327ca971d917a6b849eff3b2cca9aaecd33a17
RahulRST <srahuljayaraj@outlook.com>
2022-10-23 21:41:58 +0530

    api

---
+++ src/api/api.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask("Flaskend")
+
+# camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
+# for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+# for local webcam use cv2.VideoCapture(0)
+
+camera = cv2.VideoCapture(0)
+
+def frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video')
+def video_feed():
+    return Response(frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
