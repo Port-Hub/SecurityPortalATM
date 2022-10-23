68667080c9805171734078f972a14700f5854a9a
Adelin-Brianna <adelinbrianna15@gmail.com>
2022-10-23 10:33:46 +0530

    hi

--- src/pages/atmcam.jsx
+++ src/pages/atmcam.jsx
@@ -1,9 +1,15 @@
 import Layout from "../components/layout"
-
+import { Link } from "react-router-dom"
 const ATMCam = () => {
     return(
         <Layout>
-            <h1>ATMCam</h1>
+            <div>
+                <h3 className="text-2xl text-center">ATMCAM</h3>
+            </div>
+            <img src="http://localhost:5000/video"/>
+            <div className="flex flex-row text-center">
+                <Link to="/" className="rounded-lg w-full py-2 px-6 bg-sky-300">Log out</Link>
+            </div>
         </Layout>
     )
 }
--- src/pages/cctv.jsx
+++ src/pages/cctv.jsx
@@ -1,9 +1,15 @@
 import Layout from "../components/layout"
-
+import { Link } from "react-router-dom"
 const CCTV = () => {
     return(
         <Layout>
-            <h1>CCTV</h1>
+            <div>
+                <h3 className="text-2xl text-center">CCTV</h3>
+            </div>
+            <img src="http://localhost:5000/video"/>
+            <div className="flex flex-row text-center">
+                <Link to="/" className="rounded-lg w-full py-2 px-6 bg-sky-300">Log out</Link>
+            </div>
         </Layout>
     )
 }
