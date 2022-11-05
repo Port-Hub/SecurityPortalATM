ab65503cbb5a7fcad959b3341c1ee69bd06e281d
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-11-05 12:20:22 +0530

    Front_end

--- src/api/api.py
+++ src/api/api.py
@@ -13,6 +13,98 @@ sus_index = 0
 # camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
 # for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
+def analysis():
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    classifier =load_model(r'model.h5')
+    emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+    cap = cv2.VideoCapture(0)
+    global sus_index
+    Sec = 0
+    Min = 0
+    Check = 1
+    Counter = 1
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            labels = []
+            label = 'a'
+            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+            faces1 = face_cascade.detectMultiScale(gray)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces1:
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+                roi_gray = gray[y:y+h,x:x+w]
+                roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+
+                if np.sum([roi_gray])!=0:
+                    roi = roi_gray.astype('float')/255.0
+                    roi = img_to_array(roi)
+                    roi = np.expand_dims(roi,axis=0)
+
+                    prediction = classifier.predict(roi)[0]
+                    label=emotion_labels[prediction.argmax()]
+                    label_position = (x,y)
+                    cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                    print(label)
+                else:
+                    cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)     
+
+            if len(faces) > 0:  
+
+                Sec += 1
+                print(str(Min) + " Mins " + str(Sec) + " Sec ")
+
+                cv2.putText(frame, "Time: " + str(Min) + " Mins " + str(Sec) + " Sec ", (0,frame.shape[0] -30), cv2.FONT_HERSHEY_TRIPLEX, 0.5,  (0,0,255), 1)
+                cv2.putText(frame, "Number of faces detected: " + str(faces.shape[0]), (0,frame.shape[0] -10), cv2.FONT_HERSHEY_TRIPLEX, 0.5,  (0,0,255), 1)    
+
+                time.sleep(1)
+                if Sec == 60:
+                    Sec = 0
+                    Min += 1
+                    print(str(Min) + " Minute")
+
+                if Min == 2:
+                    sus_index +=1
+                    print("Alert")
+                    if Check == 1:
+                        print("Suspicious activity detected inside ATM.")
+                        Check += 1   
+
+            if len(faces) > 3 and Counter == 1:
+                print("Suspicious activity detected inside ATM.")
+                Counter += 1
+                sus_index +=1
+
+                        
+            if len(faces) == 0:
+
+                print('No face detected')
+                cv2.putText(frame, "No face detected ", (0,frame.shape[0] -10), cv2.FONT_HERSHEY_TRIPLEX, 0.5,  (0,0,255), 1)        
+                Sec = 0
+                Min = 0
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
+                break  
+            frame = buffer.tobytes()    
+            yield (b'--frame\r\n'
+                            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    cap.release()
+    cv2.destroyAllWindows()     
+
+
 
 def timepeopleCounter():
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -160,7 +252,7 @@ def atmframes():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/mood')
 def moodfeed():
-    return Response(moodDetection(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(analysis(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/cctv')
 def cctvfeed():
     return Response(cctvframes(), mimetype='multipart/x-mixed-replace; boundary=frame')
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,10 @@
 const Layout = (props) => {
     return (
-        <div className="flex flex-col h-screen top-1/2 items-center justify-center">
-            <form className=" bg-gradient-to-r from-sky-900 to-sky-900 space-y-8 rounded-2xl p-20 shadow-2xl shadow-sky-400">
+        <div className=" font-mono flex flex-col h-screen top-1/2 items-center justify-center bg-[#FDEEDC]">
+            <div className=" truncate">
+            <h1 className="mb-5 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Bank of Baroda Security System</h1>
+            </div>
+            <form className=" bg-[#F1A661] space-y-8 rounded-2xl p-20 shadow-2xl shadow-neutral">
                 {props.children}
             </form>
         </div>
