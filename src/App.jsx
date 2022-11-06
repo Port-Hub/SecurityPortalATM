c955f93ccfa3ffbaa5584c3ed0fb32d494f1f575
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-11-06 14:41:54 +0530

    bug roboflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import ATMCam from "./pages/atmcam";
 import CCTV from "./pages/cctv";
 import Login from "./pages/login";
+import Anomaly from "./pages/anomaly"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route exact path="/" element={<Login />} />
         <Route exact path="/cctv" element={<CCTV />} />
         <Route exact path="/atmcam" element={<ATMCam />} />
+        <Route exact path="/robo" element={<Anomaly />} />
       </Routes>
     </Router>
   )
